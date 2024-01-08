d7e0420a67e8d2bac2c1bce0813d602d5c0327a0
Atanas <aatanasgd@gmail.com>
2024-01-08 13:53:55 +0200

    .

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,18 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main class for starting the Spring Boot application.
+ */
 @SpringBootApplication
 public class DemoApplication {
 
+	/**
+	 * The main method to start the Spring Boot application.
+	 *
+	 * @param args Command line arguments.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -3,8 +3,17 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controller for handling requests.
+ */
 @RestController
 public class HelloController {
+
+    /**
+     * Handles GET requests for the root path ("/").
+     *
+     * @return Greeting message from Spring Boot.
+     */
     @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
